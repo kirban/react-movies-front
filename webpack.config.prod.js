48bab65aaa1f6b6ea0df5d845dc1dd3684881b94
Kirill Romanov <kirill_romanov2@epam.com>
2021-09-20 15:41:22 +0300

    feat: configure webpack for production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,38 @@
 const path = require('path')
 
 module.exports = {
-    mode: 'production'
+    mode: 'production',
+    entry: ['./src/index.js'],
+    output: {
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist' , 'production')
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, './src'), 'node_modules'],
+        extensions: ['.js', '.jsx', '.json']
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 }
