38080b862d33ad74447a792b7c4d477b05dc41c5
kirill-romanov <89778429+kirill-romanov@users.noreply.github.com>
2021-09-27 00:33:42 +0300

    feat: implement header base

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { SearchInput } from '@components';
+import logo from '../../logo.svg';
+import '@styles/header.scss';
 
 export default class Header extends React.Component {
     render() {
         return (
-            <>
-                <p>Hello world!</p>
-            </>
+            <header>
+                <div className="headerBg"></div>
+                <div className="headerContent-top">
+                    <img src={logo} alt="App Logo" className="logo" />
+                    <button className="btn addMovieBtn">+ add movie</button>
+                </div>
+                <div className="headerContent-main">
+                    <h1>Find your movie</h1>
+                    <SearchInput />
+                </div>
+            </header>
         )
     }
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,12 @@
 import Header from "@components/Header"
 import MoviesList from "@components/MoviesList"
 import Footer from "@components/Footer"
+import SearchInput from "@components/SearchInput"
+
 
 export {
     Header,
     MoviesList,
     Footer,
+    SearchInput,
 }
