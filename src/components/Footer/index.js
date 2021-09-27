7b250cdb2332cb8de9cc07dcf066dee2031242d5
kirill-romanov <89778429+kirill-romanov@users.noreply.github.com>
2021-09-27 12:57:11 +0300

    feat: implement footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import logo from '../../logo.svg';
+import "@styles/footer.scss";
 
 export default class Footer extends React.Component {
     render() {
-        return (<div></div>)
+        return (
+            <footer>
+                <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+                    <img src={logo} alt="App Logo" className="logo" />
+                </a>
+            </footer>
+        )
     }
 }
