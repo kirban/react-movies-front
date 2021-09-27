bc3d3871e67592d044bfb8c3eea9631d7d2dca9a
kirill-romanov <89778429+kirill-romanov@users.noreply.github.com>
2021-09-27 12:51:52 +0300

    feat: implement movies cards grid

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <>
       <Header />
-      <div className="content">
+      <main className="content">
         <MoviesList />
-      </div>
+      </main>
       <Footer />
     </>
   );
