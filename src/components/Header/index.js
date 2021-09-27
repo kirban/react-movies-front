4ded90cbea1cff36d99d786bda486a7ec1744afd
kirill-romanov <89778429+kirill-romanov@users.noreply.github.com>
2021-09-27 11:32:21 +0300

    fix: problem with controls availability (because of absolute position)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,17 +6,19 @@ import '@styles/header.scss';
 export default class Header extends React.Component {
     render() {
         return (
-            <header>
-                <div className="headerBg"></div>
-                <div className="headerContent-top">
-                    <img src={logo} alt="App Logo" className="logo" />
-                    <button className="btn addMovieBtn">+ add movie</button>
-                </div>
-                <div className="headerContent-main">
-                    <h1>Find your movie</h1>
-                    <SearchInput />
-                </div>
-            </header>
+            <>  
+                <header>
+                    <div className="headerBg"></div>
+                    <div className="headerContent-top">
+                        <img src={logo} alt="App Logo" className="logo" />
+                        <button className="btn addMovieBtn">+ add movie</button>
+                    </div>
+                    <div className="headerContent-main">
+                        <h1>Find your movie</h1>
+                        <SearchInput />
+                    </div>
+                </header>
+            </>
         )
     }
 }
