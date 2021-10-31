75d05577dce691eb6832d95cce2041f4c4e11064
kirill-romanov <89778429+kirill-romanov@users.noreply.github.com>
2021-10-31 22:38:26 +0300

    refactor: make SearchInput funcitonal

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css';
 
-export default class SearchInput extends React.PureComponent {
-  render() {
-    return (
-      <form action="search.php">
-        <input className="searchInput" type="text" name="search" id="" placeholder="What do you want to watch?" />
-        <input className="searchButton" type="submit" value="Search" />
-      </form>
-    );
-  }
+const SearchInput = () => {
+  return (
+    <form action="search.php">
+      <input className="searchInput" type="text" name="search" id="" placeholder="What do you want to watch?" />
+      <input className="searchButton" type="submit" value="Search" />
+    </form>
+  );
 }
+
+export default SearchInput
