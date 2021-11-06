f3d632725ac88adfb6963b49cf50b24b23857117
kirill-romanov <89778429+kirill-romanov@users.noreply.github.com>
2021-11-07 00:16:27 +0300

    feat: connect redux with ModalForm component

--- src/components/ModalForm/index.js
+++ src/components/ModalForm/index.js
@@ -1,68 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
 import { genres } from '../../constant'
 import '@styles/modalForm.scss'
-import mockedMoviesList from "../../mocks/movies"
 
-const ModalForm = ({ movieId }) => {
-    const initialMovie = {
-        id: "0",
-        poster_path: "",
-        title: "",
-        release_date: "",
-        url: "",
-        vote_average: 0,
-        genres: [],
-        runtime: 0,
-        overview: ""
-    }
-    const [movie, setMovie] = useState(initialMovie)
-
-    useEffect(() => {
-        if (movieId) {
-            const movie = mockedMoviesList.find(movie => movie.id === movieId)
-            setMovie(movie)
-        }
-        return () => {
-            setMovie(initialMovie)
-        }
-    }, [])
-
+const ModalForm = ({ movieData }) => {
     const handleSubmit = e => {}
 
-    const handleChange = e => {
-        const inputName = e.target.name;
-
-        if (e.target.nodeName === "SELECT" && e.target.name === "genre") {
-            const { selectedOptions } = e.target
-            const selectedOptionsValues = Array.prototype.map.call(selectedOptions, option => option.value)
-            setMovie({ ...movie, genres: selectedOptionsValues })
-        } else {
-            setMovie({ ...movie, [inputName]: e.target.value })
-        }
-    }
+    const handleChange = e => {}
 
     return(
         <form onSubmit={handleSubmit}>
             <div className="formControl">
                 <label htmlFor="title">Title</label>
-                <input type="text" name="title" id="title" value={movie.title} onChange={handleChange}/>
+                <input type="text" name="title" id="title" value={movieData.title} onChange={handleChange}/>
             </div>
             <div className="formControl">
                 <label htmlFor="release_date">Release Date</label>
-                <input type="date" name="release_date" id="release_date" value={movie.release_date} onChange={handleChange}/>
+                <input type="date" name="release_date" id="release_date" value={movieData.release_date} onChange={handleChange}/>
             </div>
             <div className="formControl">
                 <label htmlFor="url">Movie URL</label>
-                <input type="url" name="url" id="url" value={movie.url} onChange={handleChange}/>
+                <input type="url" name="url" id="url" value={movieData.url} onChange={handleChange}/>
             </div>
             <div className="formControl">
                 <label htmlFor="vote_average">vote_average</label>
-                <input type="number" name="vote_average" id="vote_average" min="0" max="10" step="0.1" value={movie.vote_average} onChange={handleChange}/>
+                <input type="number" name="vote_average" id="vote_average" min="0" max="10" step="0.1" value={movieData.vote_average} onChange={handleChange}/>
             </div>
             <div className="formControl">
                 <label htmlFor="genre">Genre</label>
-                <select name="genre" id="genre" multiple onChange={handleChange} value={movie.genre}>
+                <select name="genre" id="genre" multiple onChange={handleChange} value={movieData.genre}>
                     <option defaultValue disabled>Select Genre</option>
                     {
                         genres.map((genre, idx) => 
@@ -72,18 +39,22 @@ const ModalForm = ({ movieId }) => {
             </div>
             <div className="formControl">
                 <label htmlFor="runtime">Runtime</label>
-                <input type="number" name="runtime" id="runtime" value={movie.runtime} onChange={handleChange}/>
+                <input type="number" name="runtime" id="runtime" value={movieData.runtime} onChange={handleChange}/>
             </div>
             <div className="formControl w100">
                 <label htmlFor="overview">Overview</label>
-                <textarea name="overview" id="overview" value={movie.overview} onChange={handleChange}></textarea>
+                <textarea name="overview" id="overview" value={movieData.overview} onChange={handleChange}></textarea>
             </div>
         </form>
     )
 }
 
 ModalForm.propTypes = {
-    movieId: PropTypes.number
+    movieData: PropTypes.object
 }
 
-export default ModalForm
+const mapStateToProps = state => ({
+    movieData: state.modal.movieData,
+});
+
+export default connect(mapStateToProps, null)(ModalForm)
