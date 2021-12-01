58e1f636ec2c558c3df3e92b2b4f3bf93ecf81fd
kirill-romanov <89778429+kirill-romanov@users.noreply.github.com>
2021-12-01 17:59:51 +0300

    test(e2e): valid selecting movie from search results

--- tests/e2e/SearchedMovieIsSelected.test.js
+++ tests/e2e/SearchedMovieIsSelected.test.js
@@ -1,10 +1,11 @@
 describe('searched movie selection use-case', () => {
+    const searchValue = 'Marvel'
     it('should visit home page and redirects to empty search', () => {
         cy.visit('/')
         cy.url().should('match', /search/)
     })
     it('should type search query into search input and show search results matching query', () => {
-        const searchValue = 'Marvel'
+        
         cy.focused()
             .should('have.class', 'searchInput')
         
@@ -15,9 +16,25 @@ describe('searched movie selection use-case', () => {
 
         cy.get('.cardContentTitle')
             .each(title => {
-                cy
-                    .wrap(title)
+                cy.wrap(title)
                     .should('contain.text', searchValue)
             })
+    });
+
+    it('should select movie item successfully', () => {
+        let clickedMovieTitle;
+
+        cy.get('.cardContentTitle')
+            .first()
+            .should((titleDiv) => { clickedMovieTitle = titleDiv.text() })
+            .click()
+        
+        cy.get('.movieDetailsHeading')
+            .should((heading) => {
+                expect(clickedMovieTitle).to.be.equal(heading.text())
+            })
+
+        cy.get('.movieDetails')
+            .should('be.visible')
     });
 })
