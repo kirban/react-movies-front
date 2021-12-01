43d9d16ace78515d201b013a31c0d0b068db88c8
kirill-romanov <89778429+kirill-romanov@users.noreply.github.com>
2021-12-01 15:47:48 +0300

    refactor: rearrange e2e folder structure

---
+++ tests/e2e/SearchedMovieIsSelected.test.js
@@ -0,0 +1,16 @@
+describe('movie selection use-case', () => {
+    it('should visit home page and redirects to empty search', () => {
+        cy.visit('/')
+        cy.url().should('match', /search/)
+    })
+    it('should type search query into search input and show search results matching query', () => {
+        const searchValue = 'Marvel'
+        cy.focused()
+            .should('have.class', 'searchInput')
+        
+        cy.get('.searchInput')
+            .type(searchValue)
+            .should('have.value', searchValue)
+            .type('{enter}')
+    });
+})
